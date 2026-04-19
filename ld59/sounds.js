11e3d56f3310f7b9d84a9fa26c1c541eea7f5b93
Toby Cathcart Burn <tcathcartburn@gmail.com>
2026-04-19 22:30:15 +0100

    ld59: update sounds

--- ld59/sounds.js
+++ ld59/sounds.js
@@ -1,8 +1,25 @@
 "use strict";
 
+
+function makeDistortionCurve(amount) {
+  const k = typeof amount === "number" ? amount : 50;
+  const numSamples = 44100;
+  const curve = new Float32Array(numSamples);
+  const deg = Math.PI / 180;
+
+  for (let i = 0; i < numSamples; i++) {
+    const x = (i * 2) / numSamples - 1;
+    curve[i] = ((3 + k) * x * 20 * deg) / (Math.PI + k * Math.abs(x));
+  }
+  return curve;
+}
+
 let i=0
+let sineshape = new BiquadFilterNode(audio,{"type":"lowpass","Q":0.1,"frequency":2000})
+sineshape.connect(audio.destination)
+
 let sinevol= new GainNode(audio,{gain:0.4})
-sinevol.connect(audio.destination)
+sinevol.connect(sineshape)
 let sine = new OscillatorNode(audio,{frequency:400+40*i})
 sine.connect(sinevol)
 sine.start()
@@ -15,7 +32,7 @@ let soundLoopID
 function soundLoop(){
   i+=1
   let ruler = f(i)
-  sine.frequency.setTargetAtTime(200+40*ruler,audio.currentTime,0.05)
+  sine.frequency.setValueAtTime(200+40*ruler,audio.currentTime,0.05)
   sinevol.gain.setValueAtTime(0.4+Math.sqrt(ruler)*0.1,audio.currentTime) // this causes clicks, but that's reminiscent of train sounds
   sinevol.gain.setTargetAtTime(0,audio.currentTime,0.05)
   soundLoopID = setTimeout(soundLoop,tickrate/8)
